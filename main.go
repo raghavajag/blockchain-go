987ab8dcd7593f90485191163c576c5fc72a8e41
raghav-crypto <raghavjgarwal@gmail.com>
2023-05-07 22:04:24 +0530

    proof of work

--- main.go
+++ main.go
@@ -4,8 +4,16 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"fmt"
+	"math"
+	"math/big"
 	"strconv"
 	"time"
+)
+
+const targetBits = 14
+
+var (
+	maxNonce = math.MaxInt64
 )
 
 type Block struct {
@@ -13,9 +21,15 @@ type Block struct {
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
+	Nonce         int
 }
 type Blockchain struct {
 	blocks []*Block
+}
+
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
 }
 
 func (bc *Blockchain) AddBlock(data string) {
@@ -32,8 +46,11 @@ func (b *Block) SetHash() {
 }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+	block.Hash = hash[:]
+	block.Nonce = nonce
 	return block
 }
 
@@ -42,6 +59,59 @@ func NewGenesisBlock() *Block {
 }
 func NewBlockchain() *Blockchain {
 	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+	fmt.Printf("target %b\n", target)
+	pow := &ProofOfWork{b, target}
+
+	return pow
+}
+func IntToHex(n int64) []byte {
+	return []byte(strconv.FormatInt(n, 16))
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+
+	return data
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+
+		hash = sha256.Sum256(data)
+
+		fmt.Printf("\r%x", hash)
+
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			// fmt.Printf("hash %s\n", hashInt.String()) // decimal hash
+			// fmt.Printf("target %s\n", pow.target) // decimal target
+			nonce++
+		}
+		// break
+	}
+	fmt.Print("\n\n")
+	return nonce, hash[:]
 }
 func main() {
 	bc := NewBlockchain()
